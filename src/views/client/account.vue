a9a0809fafe771ae361c1eb4f7ecf74052e065a3
davislien <davislien@163.com>
2018-01-26 09:53:52 +0800

    详情页标题样式

--- src/views/client/account.vue
+++ src/views/client/account.vue
@@ -1,26 +1,46 @@
 <template>
-  <div class="main-content">
-    账户信息
+  <div class="client-account main-content">
+    <lh-title title="账户信息"></lh-title>
+
+    <div class="detail-content">
+      <div class="grid-title">
+        基础信息
+        <!-- <el-button class="ml10" type="primary" size="mini">短信通知</el-button> -->
+      </div>
+
+      <div class="head-content">
+        <qr-code
+          :text="qrUrl"
+          :size="80"
+          error-level="L"></qr-code></div>
+
+        <a
+          :href="qrUrl"
+          download="公司名+二维码">下载二维码</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import qrCode from 'vue-qrcode-component'
   export default {
-    name: 'Account',
-    data() {
-      return {}
+    mixins: [],
+    components: { qrCode },
+    data () {
+      return {
+        qrUrl: 'http://shared-office.oss-cn-shenzhen.aliyuncs.com/shared-office/img/20171128/20171128155642227pVi.jpg?x-oss-process=image/resize,m_lfit,h_80,w_80'
+      }
     },
     props: {},
-    components: {},
-    mounted() {
-      this.$nextTick(() => {
-      })
-    },
     watch: {},
     computed: {},
-    filters: {},
+    mounted() {},
     methods: {}
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .client-account {
+  }
+</style>
