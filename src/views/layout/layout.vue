f6763dcd448ec014d7136aa841e4eecfe1fe35b0
davislien <davislien@163.com>
2018-02-08 18:02:45 +0800

    全局添加置顶功能

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -15,6 +15,16 @@
         </transition>
       </section>
 
+      <!--可自定义按钮的样式、show/hide临界点、返回的位置  -->
+      <!--如需文字提示，可在外部添加element的<el-tooltip></el-tooltip>元素  -->
+      <el-tooltip placement="top" content="置顶" effect="light">
+        <scrool-top
+          transitionName="fade"
+          :customStyle="myBackToTopStyle"
+          :visibilityHeight="300"
+          :backPosition="50"></scrool-top>
+      </el-tooltip>
+
       <p class="layout-footer theme-gray">Copyright © {{ year }} 广州雷猴软件开发有限公司</p>
     </div>
   </div>
@@ -24,11 +34,21 @@
   import { mapGetters } from 'vuex'
   import navbar from './navbar'
   import sidebar from './sidebar'
+  import scroolTop from '@/components/scrool-top'
 
   export default {
-    components: { navbar, sidebar },
+    components: { navbar, sidebar, scroolTop },
     data () {
       return {
+        myBackToTopStyle: {
+          right: '50px',
+          bottom: '50px',
+          width: '40px',
+          height: '40px',
+          'border-radius': '4px',
+          'line-height': '45px', // 请保持与高度一致以垂直居中 Please keep consistent with height to center vertically
+          background: '#e7eaf1'// 按钮的背景颜色 The background color of the button
+        },
         year: (new Date()).getFullYear()
       }
     },
