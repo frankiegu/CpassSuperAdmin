5411dddbddd0988c9f07a97784d98979a8bf5cb6
davislien <davislien@163.com>
2018-03-05 16:31:53 +0800

    SOSP-16 完成场地管理静态页

--- src/views/space/list.mixins.js
+++ src/views/space/list.mixins.js
@@ -1,92 +1,20 @@
-import { API_PATH } from '@/config/env'
-import tableMixins from '@/mixins/table'
-import option from '@/components/option'
-import pickerOptions from '@/mixins/pickerOptions'
-import { formatTimeString, downloadFile } from '@/config/utils'
-import { clientList } from '@/service/client'
-
 export default {
-  mixins: [tableMixins, pickerOptions],
-  components: {
-    [option.name]: option
-  },
   data () {
     return {
-      formData: {
-        name: '',
-        reg_date: null,
-        registerWay: '',
-        validaty: '',
-        productStatus: ''
-      },
-      channels: [
-        { id: 1, channel: '后台创建' },
-        { id: 2, channel: '客户自助注册' }
-      ],
-      timeValidity: [
-        { id: 1, validity: '永久' },
-        { id: 2, validity: '一周内到期' },
-        { id: 3, validity: '一月内到期' }
-      ],
       statusList: [
-        { value: ' ', label: '全部' },
-        { value: 1, label: '正常', type: 'success' },
-        { value: 0, label: '停用', type: 'danger' }
+        { val: 5, text: '待支付' },
+        { val: 10, text: '待使用' },
+        { val: 20, text: '已使用' },
+        { val: 30, text: '已取消' },
+        { val: 40, text: '待退款' },
+        { val: 50, text: '已退款' }
       ]
     }
   },
-  mounted () {
-    this.getPageData()
-  },
-  methods: {
-    formatTime(time) {
-      return !time ? '' : time.replace(/:\d{2}$/, '')
-    },
-    getPageData() {
-      const formData = this.formData
-      const paramsObj = {
-        pageSize: this.pageSize,
-        pageNum: this.currentPage,
-        name: formData.name,
-        validaty: formData.validaty,
-        registerWay: formData.registerWay,
-        productStatus: formData.productStatus,
-        createStartDate: this.formData.reg_date ? formatTimeString(this.formData.reg_date[0]) : null,
-        createEndDate: this.formData.reg_date ? formatTimeString(this.formData.reg_date[1]) : null
-      }
-
-      clientList(paramsObj).then(res => {
-        if (res.status === 'true') {
-          let data = res.info
-          if (data) {
-            this.pageTotal = data.total
-            this.tableData = data.result
-          }
-
-          this.tableLoading = false
-          if (this.tableData.length === 0) {
-            this.tableEmpty = '暂时无数据'
-          }
-        } else {
-          this.setMsg('error', res.msg)
-        }
-      })
-    },
-    exportExcel() {
-      if (!this.tableData.length) {
-        return this.setMsg('暂无数据')
-      }
-      const formData = this.formData
-      const downParams = {
-        name: formData.name,
-        validaty: formData.validaty,
-        registerWay: formData.registerWay,
-        productStatus: formData.productStatus,
-        createStartDate: formData.reg_date ? formatTimeString(formData.reg_date[0]) : null,
-        createEndDate: formData.reg_date ? formatTimeString(formData.reg_date[1]) : null
-      }
-      let url = API_PATH + '/supervisor/client/exportAll'
-      downloadFile(url, downParams)
-    }
-  }
+  props: {},
+  components: {},
+  mounted() {},
+  watch: {},
+  computed: {},
+  methods: {}
 }
