7452f6788fbc4c942b94c8cc2e99aa02da9ce8a8
连明堂 <davilien@163.com>
2018-05-07 11:07:02 +0800

    同步后台管理代码到总后台

--- src/views/field/detail.mixins.js
+++ src/views/field/detail.mixins.js
@@ -15,6 +15,10 @@ export default {
       equipments: {},
       fieldImgs: {},
 
+      // 该场地，对外开放设置的状态
+      // 跟场地的开放状态不同
+      openSettingStatus: false,
+
       typeList: [
         { key: '0', label: '全部' },
         { key: '1', label: '会议室' },
@@ -30,30 +34,36 @@ export default {
       // 整周
       openWeek: [{
         type: 1,
-        status: 1,
-        workState: 1,
-        restState: 1,
+        status: 0,
         startTime: '',
         endTime: '',
-        price: '' // 收费标准
+        price: '', // 收费标准
+        workState: 1,
+        restState: 1,
+        longTime: 0,
+        itmeType: 1
       }],
       // 基本价格，工作日、非工作日
       openPeriod: [{
         type: 1,
-        status: 1,
+        status: 0,
         startTime: '',
         endTime: '',
         price: '', // 收费标准
         workState: 1,
-        restState: 1
+        restState: 1,
+        longTime: 0,
+        itmeType: 1
       }, {
         type: 2,
-        status: 1,
+        status: 0, // 休息的情况下，接口返回为空，默认休息更合理
         startTime: '',
         endTime: '',
         price: '', // 收费标准
         workState: 1,
-        restState: 1
+        restState: 1,
+        longTime: 0,
+        itmeType: 1
       }],
       openOffice: [], // 办公室
 
@@ -64,8 +74,11 @@ export default {
         itmeType: '', // 0 免费时段 1普通时段 2特殊时段
         startTime: '',
         endTime: '',
-        longTime: '',
+        longTime: 0,
+        workState: 1,
+        restState: 1,
         price: '',
+        clientType: '',
         appointmentTimeType: '' // 是否区分工作日 1:不区分 2:区分
       }
     }
--- src/views/field/detail.vue
+++ src/views/field/detail.vue
@@ -68,7 +68,7 @@
     <!-- 预约设置 -->
     <lh-card-empty
       :cardTitle="fieldType === '1' || fieldType === '4' ? '预约设置' : '开放时段'">
-      <h3 v-if="fieldType === '1' || fieldType === '4'" class="text-title fz16 mb6 mt0 no-required">基本价格</h3>
+      <h3 class="text-title fz16 mb6 mt0 no-required">基本价格</h3>
 
       <!-- 非办公室 -->
       <el-table border v-if="fieldType !== '5'" :data="openData" class="width100">
@@ -84,7 +84,7 @@
 
         <el-table-column label="状态">
           <template slot-scope="scope">
-            <span v-if="scope.row.status">
+            <span v-if="appointmentTimeType === 2">
               <span v-if="scope.row.status === 1">营业</span>
               <span v-else>休息</span>
             </span>
@@ -101,7 +101,7 @@
 
         <el-table-column label="收费标准">
           <template slot-scope="scope">
-            {{ scope.row.price ? scope.row.price + (fieldType === 1 ? '元/小时' : '元/天') : '-' }}
+            {{ scope.row.price || scope.row.price === 0 ? scope.row.price + (fieldType === 1 ? '元/小时' : '元/天') : '-' }}
           </template>
         </el-table-column>
       </el-table>
@@ -123,47 +123,55 @@
       </el-table>
 
       <!-- @#注意：工位和办公室没有对外价格 -->
-      <h3 v-if="fieldType === '1' || fieldType === '4'" class="text-title fz16 mb6 mt24 no-required">对外价格</h3>
+      <h3 v-if="fieldType !== '5'" class="text-title fz16 mb6 mt24 no-required">对外价格</h3>
       <el-table border
-        v-if="fieldType === '1' || fieldType === '4'"
+        v-if="fieldType !== '5'"
         :data="openPrice"
         class="width100">
         <el-table-column label="日期">
           <template slot-scope="scope">
-            <span v-if="scope.row.type === 1">工作日</span>
-            <span v-else-if="scope.row.type === 2">非工作日</span>
+            <span v-if="appointmentTimeType === 2">
+              <span v-if="scope.row.type === 1">工作日</span>
+              <span v-else-if="scope.row.type === 2">非工作日</span>
+            </span>
             <span v-else>整周</span>
           </template>
         </el-table-column>
 
         <!-- 休息的时段不返回，对外价格都是营业的，休息没有对外 -->
         <el-table-column label="状态">
-          <template slot-scope="scope">营业</template>
+          <template slot-scope="scope">
+            <span v-if="appointmentTimeType === 2">
+              <span v-if="scope.row.status === 1">营业</span>
+              <span v-else>休息</span>
+            </span>
+            <span v-else>营业</span>
+          </template>
         </el-table-column>
 
-        <el-table-column>
+        <el-table-column v-if="fieldType !== '3'">
           <template slot-scope="scope">
-            <span v-if="scope.row.itmeType === '1'">普通时段</span>
-            <span v-else-if="scope.row.itmeType === '2'">特殊时段</span>
+            <span v-if="scope.row.itmeType === 1">普通时段</span>
+            <span v-else-if="scope.row.itmeType === 2">特殊时段</span>
             <span v-else>免费时段</span>
           </template>
         </el-table-column>
 
-        <el-table-column label="时段">
+        <el-table-column label="时段" v-if="fieldType !== '3'">
           <template slot-scope="scope">
             {{ scope.row.startTime ? scope.row.startTime + '-' + scope.row.endTime : '-' }}
           </template>
         </el-table-column>
 
-        <el-table-column label="时长">
+        <el-table-column label="时长" v-if="fieldType !== '3'">
           <template slot-scope="scope">
-            {{ scope.row.longTime }}小时
+            {{ scope.row.longTime ? scope.row.longTime + '小时' : '-' }}
           </template>
         </el-table-column>
 
         <el-table-column label="价格">
           <template slot-scope="scope">
-            ¥{{ scope.row.price }}
+            {{ scope.row.price || scope.row.price === 0 ? '¥' + scope.row.price : '-' }}
           </template>
         </el-table-column>
       </el-table>
@@ -207,60 +215,50 @@ export default {
     this.getPageData()
   },
   methods: {
-    //
-    turnOpenData(ajaxData, fieldPriceList) {
-      // console.log('ajaxData', ajaxData, fieldPriceList);
-
+    turnOpenData(ajaxData) {
+      // console.log('ajaxData', ajaxData, this.openWeek);
       // 基本价格
-      // ------------------
-      if (this.appointmentTimeType === 1) {
-        this.openWeek[0].status = ajaxData.workState
-        this.openWeek[0].price = ajaxData.workPriceDetailList[0].price
-
-        if (this.fieldType !== '3') {
-          this.openWeek[0].startTime = ajaxData.workPriceDetailList[0].startTime
-          this.openWeek[0].endTime = ajaxData.workPriceDetailList[0].endTime
-        }
-        // console.log('openWeek', this.openWeek);
-      } else {
-        if (this.fieldType !== '3') {
-          // 工作日
-          this.openPeriod[0].status = ajaxData.workState
-          this.openPeriod[0].price = ajaxData.workPriceDetailList[0].price
+      if (ajaxData.appointmentTimeType) {
+        if (this.appointmentTimeType === 1) {
           if (this.fieldType !== '3') {
-            this.openPeriod[0].startTime = ajaxData.workPriceDetailList[0].startTime
-            this.openPeriod[0].endTime = ajaxData.workPriceDetailList[0].endTime
-          }
+            if (ajaxData.workState) {
+              this.openWeek[0].status = ajaxData.workState
+              this.openWeek[0].price = ajaxData.workPriceDetailList[0].price
+              this.openWeek[0].startTime = ajaxData.workPriceDetailList[0].startTime
+              this.openWeek[0].endTime = ajaxData.workPriceDetailList[0].endTime
+            }
 
-          if (ajaxData.appointmentTimeType === 1) {
             this.openPeriod[0].type = 1
-          } else {
+          }
+          // console.log('openWeek', this.openWeek);
+        } else {
+          // @#注意：区分工作日的情况，工作日和非工作日都有可能是休息的状态
+          if (this.fieldType !== '3') {
+            // 工作日
+            if (ajaxData.workState) {
+              this.openPeriod[0].status = ajaxData.workState
+              this.openPeriod[0].price = ajaxData.workPriceDetailList[0].price
+              this.openPeriod[0].startTime = ajaxData.workPriceDetailList[0].startTime
+              this.openPeriod[0].endTime = ajaxData.workPriceDetailList[0].endTime
+            }
+
             // 非工作日
-            this.openPeriod[1].type = 2
-            this.openPeriod[1].status = ajaxData.restState
-            this.openPeriod[1].price = ajaxData.restPriceDetailList[0].price
-            if (this.fieldType !== '3') {
+            if (ajaxData.restState) {
+              this.openPeriod[1].type = 2
+              this.openPeriod[1].status = ajaxData.restState
+              this.openPeriod[1].price = ajaxData.restPriceDetailList[0].price
+
               this.openPeriod[1].startTime = ajaxData.restPriceDetailList[0].startTime
               this.openPeriod[1].endTime = ajaxData.restPriceDetailList[0].endTime
             }
+            // console.log('openPeriod--------------', this.openPeriod);
           }
-
-          // console.log('openPeriod--------------', this.openPeriod);
-        } else {
-          this.openPeriod = this.turnPrice(fieldPriceList)
-          // console.log('fieldPriceList----------', this.openWeek);
         }
       }
-
-      // 只有会议室和其他类型，才有时段
-      if (this.fieldType === '1' || this.fieldType === '4') {
-        // 对外价格
-        // ------------------
-        this.openPrice = []
-
-        this.openPrice = this.turnPrice(fieldPriceList)
-        // console.log('openPrice----------', this.openPrice);
-      }
+    },
+    // 设置对外价格
+    setOpenPrice(fieldPriceList) {
+      this.openPrice = this.turnPrice(fieldPriceList)
     },
     // 排序
     turnPrice(fieldPriceList) {
@@ -268,15 +266,18 @@ export default {
       let lists = []
       for (let item of fieldPriceList) {
         if (item.clientType === 'WXAPP') {
+          // 用来判断，该场地对外开放设置，是否开放
+          list.clientType = item.clientType
+
           // 排在第一位，整周的用
           list.price = item.price
           if (item.periodOrder === 1) {
-            list.itmeType = '1'
+            list.itmeType = 1
           } else {
             if (item.price) {
-              list.itmeType = '2'
+              list.itmeType = 2
             } else {
-              list.itmeType = '0'
+              list.itmeType = 0
             }
           }
 
@@ -285,6 +286,11 @@ export default {
           } else {
             list.type = 2
           }
+
+          // 休息时段的不返回
+          // if (item.fieldType === 3) {
+          list.status = 1
+          // }
           // console.log('item.dayType-----', list.type)
 
           list.startTime = item.startTime
@@ -298,7 +304,6 @@ export default {
         }
       }
       // console.log('lists-----', lists);
-
       // @#注意，再进行冒泡排序，从小到大
       // 从1开始，第一个是普通时段
       // for (var i = 1, len = lists.length; i < len; i++) {
@@ -322,15 +327,163 @@ export default {
       }
       // console.log('openData', this.openData);
     },
+    /**
+     * 是否开放，如果没有开放，需要手动加入。
+     *
+     * 开放的时候，是否休息，休息的时候接口没有返回，需要手动加入
+     * 还要注意插入的位置，在特殊时段的情况下
+     * 还得计算时长
+     */
+    setBasePrice(priceData) {
+      // 该场地的对外开放设置为关闭时，插入基本价格
+      // 只要有第一条，就可以走下面的流程了
+      // console.log('test', this.openPeriod, this.openSettingStatus)
+
+      /**
+       * 开放，返回营业数据，休息不返回
+       * 关闭，不返回数据
+       * @#踩坑：到最后，还是需求不明确，或者是需求理解错误耽误了
+       */
+      if (!this.openSettingStatus) {
+        /**
+         * #@注意：没有对外开放的场地，不展示对外开放价格
+         */
+        // if (this.appointmentTimeType === 1) {
+        //   this.openPrice.splice(1, 0, this.openData[0])
+        //   this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+        // } else {
+        //   this.openPrice.splice(1, 0, this.openData[0])
+        //   this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+
+        //   this.openData[1].longTime = calcLongTime(this.openData[1].startTime, this.openData[1].endTime)
+        //   this.openPrice.splice(this.openPrice.length, 0, this.openData[1])
+        // }
+
+        // if (!priceData.workState) {
+        // 判断是否有对应的工作日、非工作日，返回相应地数据
+        // 而且需要区分有没有休息的情况，有才添加
+
+        /**
+         * 确定openPeriod的返回值
+         */
+        // if (this.openPrice.length) {
+        //   // 计算时长
+        //   this.openPrice.splice(1, 0, this.openPeriod[0])
+        //   this.openPeriod[0].longTime = calcLongTime(this.openPeriod[0].startTime, this.openPeriod[0].endTime)
+        // } else {
+        //   this.openPrice.splice(1, 0, this.openData[0])
+        //   this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+        // }
+        // } else {
+        // if (this.openPrice.length) {
+        //   // 计算时长
+        //   this.openPrice.splice(1, 0, this.openPeriod[0])
+        //   this.openPeriod[0].longTime = calcLongTime(this.openPeriod[0].startTime, this.openPeriod[0].endTime)
+        // } else {
+        //   console.log('this.openData[0]', this.openData[0]);
+        //   this.openPrice.splice(1, 0, this.openData[0])
+        //   this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+        // }
+        // }
+
+        // console.log('openPrice', this.openPrice);
+        // if (!priceData.restState) {
+        //   // 工作日的特殊时段结束了，再插入非工作的基本价格
+        //   for (let i = 0, len = this.openPrice.length; i < len; i++) {
+        //     if (this.openPrice[i].type === 2) {
+        //       this.openPrice.splice((i + 1), 0, this.openPeriod[1])
+        //       break;
+        //     } else {
+        //       // 计算时长
+        //       this.openPeriod[len - 1].longTime = calcLongTime(this.openPeriod[len - 1].startTime, this.openPeriod[len - 1].endTime)
+        //       this.openPrice.splice((len), 0, this.openPeriod[1])
+        //       break;
+        //     }
+        //   }
+        // } else {
+        //   // 只返回了有的数据
+        //   // if (this.openPrice.length) {
+        //   //   // 计算时长
+        //   //   this.openPrice.splice(1, 0, this.openPeriod[0])
+        //   //   this.openPeriod[0].longTime = calcLongTime(this.openPeriod[0].startTime, this.openPeriod[0].endTime)
+        //   // } else {
+        //   //   console.log('this.openData[0]', this.openData[0]);
+        //   //   this.openPrice.splice(1, 0, this.openData[0])
+        //   //   this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+        //   // }
+        // }
+      } else {
+        // if (!priceData.workState) {
+        //   this.openPrice.splice(0, 0, this.openData[0])
+        //   this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+        // }
+        // if (!priceData.restState) {
+        //   this.openData[1].longTime = calcLongTime(this.openData[1].startTime, this.openData[1].endTime)
+        //   this.openPrice.splice(this.openPrice.length, 0, this.openData[1])
+        // }
+
+        // if (!priceData.workState) {
+        //   if (this.openPrice.length) {
+        //     // 计算时长
+        //     this.openPrice.splice(1, 0, this.openPeriod[0])
+        //     this.openPeriod[0].longTime = calcLongTime(this.openPeriod[0].startTime, this.openPeriod[0].endTime)
+        //   } else {
+        //     this.openPrice.splice(1, 0, this.openData[0])
+        //     this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+        //   }
+        // }
+
+        // 开放和关闭的区别
+        // else {
+        //   if (this.openPrice.length) {
+        //     // 计算时长
+        //     this.openPrice.splice(1, 0, this.openPeriod[0])
+        //     this.openPeriod[0].longTime = calcLongTime(this.openPeriod[0].startTime, this.openPeriod[0].endTime)
+        //   } else {
+        //     console.log('this.openData[0]', this.openData[0]);
+        //     this.openPrice.splice(1, 0, this.openData[0])
+        //     this.openData[0].longTime = calcLongTime(this.openData[0].startTime, this.openData[0].endTime)
+        //   }
+        // }
+
+        // console.log('openPrice', this.openPrice);
+        // if (!priceData.restState) {
+        //   // 工作日的特殊时段结束了，再插入非工作的基本价格
+        //   for (let i = 0, len = this.openPrice.length; i < len; i++) {
+        //     if (this.openPrice[i].type === 2) {
+        //       this.openPrice.splice((i + 1), 0, this.openPeriod[1])
+        //       break;
+        //     } else {
+        //       // 计算时长
+        //       this.openPeriod[len - 1].longTime = calcLongTime(this.openPeriod[len - 1].startTime, this.openPeriod[len - 1].endTime)
+        //       this.openPrice.splice((len), 0, this.openPeriod[1])
+        //       break;
+        //     }
+        //   }
+        // }
+      }
+
+      // console.log('this.openPrice', this.openPrice);
+    },
+    getOpenStatus() {
+      for (let item of this.openPrice) {
+        // console.log('item.clientType', item.clientType);
+        if (item.clientType === 'WXAPP') {
+          this.openSettingStatus = true
+          break;
+        }
+      }
+    },
     getPageData() {
       fieldDetail({ fieldId: this.fieldId }).then(res => {
         if (res.status === 'true') {
           let resInfo = res.info
           this.field = resInfo.field
           this.space = resInfo.space
+
           if (!this.fieldType) {
             this.fieldType = resInfo.field.type + ''
-            this.setPageTitle()
+            this.setPageTitle('getTitle')
           }
           this.appointmentTimeType = resInfo.field.appointmentTimeType
           this.store = resInfo.store
@@ -340,36 +493,86 @@ export default {
           this.fieldImgs = resInfo.fieldImgs.map(item => ({
             url: item.img
           }))
+          this.fieldImgs = []
+          for (let item of resInfo.fieldImgs) {
+            // @#注意：主图不重复展示在列表中
+            if (item.mainImg === 1) {
+              this.mainImg = item.img
+            } else {
+              this.fieldImgs.push({ url: item.img })
+            }
+          }
 
           switch (this.fieldType) {
             case '1':
               this.stationNum = resInfo.fieldMeeting.maxAdmissibleNum
-              this.turnOpenData(resInfo.fieldMeeting, resInfo.fieldPriceList)
-              break;
-            case '3':
-              this.stationNum = resInfo.fieldStation.stationNum
-              this.turnOpenData(resInfo.fieldStation, resInfo.fieldPriceList)
+              this.turnOpenData(resInfo.fieldMeeting)
+              this.setOpenPrice(resInfo.fieldPriceList)
+
+              /**
+               * 拿到场地是否对外开放，先拿到openPrice
+               * 在调用turnOpenData方法之后调用
+               * 办公室不需要
+               */
+              this.getOpenStatus()
+
+              this.toggleWeek()
+
+              // 对外价格，在基本价格之后整合
+              // this.setBasePrice(resInfo.fieldMeeting)
               break;
             case '4':
               this.stationNum = resInfo.fieldOther.maxAdmissibleNum
-              this.turnOpenData(resInfo.fieldOther, resInfo.fieldPriceList)
+              this.turnOpenData(resInfo.fieldOther)
+              this.setOpenPrice(resInfo.fieldPriceList)
+
+              this.getOpenStatus()
+              this.toggleWeek()
+              // 插入基本几个
+              // this.setBasePrice(resInfo.fieldOther)
+              break;
+            case '3':
+              this.stationNum = resInfo.fieldStation.stationNum
+
+              if (this.appointmentTimeType === 1) {
+                let fieldPriceList = [...this.openWeek]
+                fieldPriceList[0].price = resInfo.fieldStation.workPrice
+                fieldPriceList[0].status = resInfo.fieldStation.workState
+                this.openWeek = [...fieldPriceList]
+              } else {
+                let fieldPriceList = [...this.openPeriod]
+                fieldPriceList[0].price = resInfo.fieldStation.workPrice
+                fieldPriceList[0].status = resInfo.fieldStation.workState
+                fieldPriceList[1].price = resInfo.fieldStation.restPrice
+                fieldPriceList[1].status = resInfo.fieldStation.restState
+                this.openPeriod = [...fieldPriceList]
+              }
+              // 拿基本价格
+              this.toggleWeek()
+
+              // 拿对外价格
+              this.setOpenPrice(resInfo.fieldPriceList)
               break;
             case '5':
               this.stationNum = resInfo.fieldOffice.stationNum
               this.openOffice = [{ ...resInfo.fieldOffice }]
+
+              this.toggleWeek()
               break;
           }
-
-          this.toggleWeek()
         }
       })
     },
-    setPageTitle() {
+    setPageTitle(type) {
       for (let item of this.typeList) {
         if (this.fieldType === item.key) {
           this.titleName = this.field.fieldName + '（' + item.label + '）'
           document.title = this.titleName
         }
+      }
+
+      if (type !== 'getTitle') {
+        this.getPageData()
       }
     },
     // 更新会员状态
