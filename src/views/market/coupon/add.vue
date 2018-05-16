fb0df1298e4239d0332334e8ce4257c8db7c93f4
jingyi_h <jingyi_h@gzleihou.cn>
2018-05-16 16:54:13 +0800

    添加核销点弹窗组件

---
+++ src/views/market/coupon/add.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="add-coupon">
+    <lh-title />
+
+    <div class="lh-card-main lh-card-body">
+    </div>
+
+    <!-- 添加核销点弹窗 -->
+    <add-wop-dialog v-if="false" :is-visible="isWopVisible" @closeDialog="isWopVisible = false"></add-wop-dialog>
+  </div>
+</template>
+
+<script>
+  import addWopDialog from '../components/add-wop-dialog'
+  export default {
+    name: 'add',
+    data() {
+      return {
+        isWopVisible: false // 添加核销点弹窗
+      }
+    },
+    components: { addWopDialog },
+    mounted() {
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
