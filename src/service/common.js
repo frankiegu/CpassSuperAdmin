e867c272d2b98e6196837d1a738792510957bb28
jingyi_h <jingyi_h@gzleihou.cn>
2018-05-17 17:53:20 +0800

    SOSP-36 公共省市区列表

--- src/service/common.js
+++ src/service/common.js
@@ -7,3 +7,6 @@ import { API_PATH } from '../config/env'
 
 // 获取系统常量值
 export const loadConstant = (key) => fetch(API_PATH + '/common/constant/load', { key }, 'POST')
+
+// 获取地址库
+export const regionList = (obj) => fetch(API_PATH + '/common/region/listAll', obj, 'POST')
--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -1,16 +1,19 @@
 import Cookies from 'js-cookie'
+import { regionList } from '@/service/common'
 
 const common = {
   state: {
     navCrumb: '', // 动态设置面包屑的内容
     sidebar: {
       opened: !+Cookies.get('sidebarStatus')
-    }
+    },
+    regionList: [] // 省市区地址列表
   },
 
   getters: {
     navCrumb: state => state.navCrumb,
-    sidebar: state => state.sidebar
+    sidebar: state => state.sidebar,
+    regionList: state => state.regionList
   },
 
   mutations: {
@@ -24,11 +27,26 @@ const common = {
         Cookies.set('sidebarStatus', 0)
       }
       state.sidebar.opened = !state.sidebar.opened
+    },
+    GET_REGION_LIST: (state, list) => {
+      state.regionList = list
     }
   },
 
   actions: {
-    toggleSideBar: ({ commit }) => commit('TOGGLE_SIDEBAR')
+    toggleSideBar: ({ commit }) => commit('TOGGLE_SIDEBAR'),
+    // 获取省市区列表
+    getRegionList: ({ commit, state }) => {
+      console.log('1')
+      if (!state.regionList.length) {
+        console.log('2')
+        regionList().then(res => {
+          if (res.status === 'true') {
+            commit('GET_REGION_LIST', res.info.children)
+          }
+        })
+      }
+    }
   }
 }
 
