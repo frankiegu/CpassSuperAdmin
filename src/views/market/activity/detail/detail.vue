d079fba8f5b6fe28afeb33f04eb428f97f3ac1e2
dengyu <1228280984@qq.com>
2018-05-30 09:57:32 +0800

    SOSP-57 活动列表静态

--- src/views/market/activity/detail/detail.vue
+++ src/views/market/activity/detail/detail.vue
@@ -87,79 +87,63 @@
       <el-tabs v-model="activeTab">
         <!-- 领取方式标签页 -->
         <el-tab-pane label="卡券信息" name="couponInformation" class="coupon-detail-info">
+          <div class="mt10">
+            <div class="senior-title">整体统计</div>
+            <div class="card-body">
+            </div>
+          </div>
         </el-tab-pane>
         <!--领券详情标签页-->
         <el-tab-pane label="领券详情" name="receiveRecord" class="receive-list">
-          <el-form :model="formData" :inline="true" class="text-right mr-10">
-            <!-- 选择的是到期时间，所以是往后选 -->
-            <el-form-item>
-              <el-date-picker
-                v-model="formData.reg_date"
-                @change="getPageData(1)"
-                type="daterange"
-                align="right"
-                clearable
-                start-placeholder="开始日期"
-                end-placeholder="结束日期"
-                placeholder="选择提交日期"
-                :picker-options="pickerOptions"></el-date-picker>
-            </el-form-item>
+          <div class="mt10">
+            <div class="senior-title">整体统计</div>
+            <div class="card-body">
+              <el-form :model="formData" :inline="true" class="text-right mr-10">
+                <!-- 选择的是到期时间，所以是往后选 -->
+                <el-form-item>
+                  <el-date-picker
+                    v-model="formData.reg_date"
+                    @change="getPageData(1)"
+                    type="daterange"
+                    align="right"
+                    clearable
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期"
+                    placeholder="选择提交日期"
+                    :picker-options="pickerOptions"></el-date-picker>
+                </el-form-item>
 
-            <el-form-item class="fr">
-              <el-button @click="exportExcel" class="lh-btn-export">
-                <lh-svg icon-class="icon-download" />导出
-              </el-button>
-            </el-form-item>
+                <el-form-item class="fr">
+                  <el-button @click="exportExcel" class="lh-btn-export">
+                    <lh-svg icon-class="icon-download" />导出
+                  </el-button>
+                </el-form-item>
 
-          </el-form>
+              </el-form>
 
-          <el-table :data="tableData" :empty-text="tableEmpty" :slot="tableEmpty" v-loading="tableLoading" border
-                    style="width: 100%">
+              <el-table :data="tableData" :empty-text="tableEmpty" :slot="tableEmpty" v-loading="tableLoading" border
+                        style="width: 100%">
 
-            <el-table-column label="活动ID" prop="name" align="left"></el-table-column>
-            <el-table-column label="活动名称" prop="name" align="left">
-              <template slot-scope="scope">
-                <router-link
-                  :to="{path: '/activity/detail', query: {id: 3210}}"
-                  class="table-link">
-                  {{ scope.row.name }}
-                </router-link>
-              </template>
-            </el-table-column>
-            <el-table-column label="活动类型" prop="price"></el-table-column>
-            <el-table-column label="活动开始时间" prop="providerName" align="left" sortable sort-by="created"></el-table-column>
-            <el-table-column label="活动结束时间" prop="providerName" align="left" sortable sort-by="created"></el-table-column>
-            <el-table-column label="活动状态">
-              <template slot-scope="scope">
-                <span v-if="scope.row.type === 1">未发布</span>
-                <span v-if="scope.row.type === 2">未开始</span>
-                <span v-if="scope.row.type === 3">进行中</span>
-                <span v-if="scope.row.type === 4">已结束</span>
-                <span v-if="scope.row.type === 5">暂停</span>
-              </template>
-            </el-table-column>
+                <el-table-column label="日期" prop="providerName" align="left" sortable sort-by="created"></el-table-column>
+                <el-table-column label="查看人数" prop="providerName" align="left"></el-table-column>
+                <el-table-column label="参与次数" prop="providerName" align="left"></el-table-column>
+                <el-table-column label="分享人数" prop="providerName" align="left"></el-table-column>
+                <el-table-column label="分享次数" prop="providerName" align="left"></el-table-column>
 
-            <el-table-column label="操作">
-              <template slot-scope="scope">
-                <el-button type="text" v-if="scope.row.statusCode === 1"
-                           class="operate-btn">编辑</el-button>
+              </el-table>
 
-                <el-button type="text" v-if="scope.row.statusCode === 1"
-                           class="operate-btn">复制</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-
-          <el-pagination
-            :total="pageTotal"
-            :layout="layoutArr"
-            :page-size="pageSize"
-            :page-sizes="pageSizeArr"
-            :current-page="currentPage"
-            class="pagination-container"
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            background></el-pagination>
+              <el-pagination
+                :total="pageTotal"
+                :layout="layoutArr"
+                :page-size="pageSize"
+                :page-sizes="pageSizeArr"
+                :current-page="currentPage"
+                class="pagination-container"
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                background></el-pagination>
+            </div>
+          </div>
         </el-tab-pane>
       </el-tabs>
     </div>
