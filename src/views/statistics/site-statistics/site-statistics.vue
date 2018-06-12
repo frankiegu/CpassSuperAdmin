1331d978da5f83e5ce4e8d89c126365d06d7c971
dengyu <1228280984@qq.com>
2018-06-12 15:37:22 +0800

    场地统计 静态

--- src/views/statistics/site-statistics/site-statistics.vue
+++ src/views/statistics/site-statistics/site-statistics.vue
@@ -7,7 +7,14 @@
         <el-col :span="8">
           <div class="modular-item">
             <div class="modular-item-left">
-
+              <span class="left-title">品牌总数</span>
+              <div class="left-count">60</div>
+            </div>
+            <div class="modular-item-right">
+              <span class="right-site">
+                近7天新增
+                <span></span>
+              </span>
             </div>
           </div>
         </el-col>
@@ -60,6 +67,39 @@
       background-color: #fff;
       border-radius: 2px;
       padding: 15px;
+
+      .modular-item-left{
+        display: inline-block;
+        width: 90px;
+        float: left;
+
+        .left-title{
+          display: inline-block;
+          color: rgba(0, 0, 0, 0.85);
+          width: 100%;
+          margin-bottom: 20px;
+        }
+        .left-count{
+          display: inline-block;
+          color: #000;
+          font-size: 28px;
+          width: 100%;
+        }
+      }
+      .modular-item-right{
+        display: inline-block;
+        width: 150px;
+        float: right;
+
+        .right-site{
+          display: inline-block;
+          width: 100%;
+          margin-bottom: 8px;
+        }
+        .right-site:last-child{
+          margin-bottom: 0px;
+        }
+      }
     }
   }
   .page-title{
