c791cf317abd52e22fcf72f2e1c7a4f62d2aa4e4
jingyi_h <jingyi_h@gzleihou.cn>
2018-06-15 18:49:33 +0800

    SOSP-95 添加编辑城市对接

--- src/views/client/city-maintain/index.vue
+++ src/views/client/city-maintain/index.vue
@@ -6,7 +6,7 @@
       <el-form :inline="true" :model="citySort" @submit.native.prevent>
         <el-button type="primary" class="fl" @click="isVisible = true">添加城市</el-button>
         <el-form-item>
-          <el-input v-model.trim="citySort.name" @keyup.native.enter="getPageData(1)" placeholder="请输入内容">
+          <el-input v-model.trim="citySort.keyword" @keyup.native.enter="getPageData(1)" placeholder="请输入内容">
             <i slot="suffix" @click="getPageData(1)" class="el-input__icon el-icon-search"></i>
           </el-input>
         </el-form-item>
@@ -14,23 +14,33 @@
     </div>
 
     <div class="lh-card-main">
-      <el-tabs v-model="citySort.country" class="lh-form-tab mb0" @tab-click="getPageData(1)">
-        <!--<el-tab-pane label="全部" key=" " name=" " />-->
-        <el-tab-pane v-for="(value, key) in countryList" :label="value" :key="key" :name="key" />
+      <el-tabs v-model="citySort.countryId" class="lh-form-tab mb0" @tab-click="getPageData(1)">
+        <el-tab-pane v-for="item in countryList" :label="item.cnName" :key="item.id" :name="item.id + ''" />
       </el-tabs>
 
       <div class="card-body">
         <el-table :data="tableData" :loading="tableLoading">
-          <el-table-column label="城市展示图" prop="imgUrl"></el-table-column>
+          <el-table-column label="城市展示图" prop="url">
+            <template slot-scope="scope">
+              <img :src="scope.row.url + zoomImgSize(50)" alt="">
+            </template>
+          </el-table-column>
+
           <el-table-column label="国家" prop="countryName"></el-table-column>
-          <el-table-column :label="citySort.country === '1' ? '省份' : '州市'" prop="provinceName"></el-table-column>
-          <el-table-column label="城市" prop="cityName"></el-table-column>
+          <el-table-column :label="citySort.countryId === '44' ? '省市' : '州市'" prop="provinceName">
+            <template slot-scope="scope">
+              <span>{{scope.row.provinceName + scope.row.cityName}}</span>
+            </template>
+          </el-table-column>
+
+          <el-table-column label="城市名称" prop="cityAliasName"></el-table-column>
           <el-table-column label="空间数" prop="spaceNum"></el-table-column>
+
           <el-table-column label="操作">
             <template slot-scope="scope">
-              <el-button type="text" class="fl">编辑</el-button>
-              <el-button type="text" v-if="scope.$index === 1" class="ml0">置顶</el-button>
-              <el-tooltip content="hello" placement="top" class="fr">
+              <el-button type="text" class="fl" @click="openEditDialog(scope.row.id)">编辑</el-button>
+              <el-button type="text" v-if="scope.$index !== 0" class="ml0">置顶</el-button>
+              <el-tooltip :content="scope.row.status === 1 ? '点击关闭城市' : '点击开启城市'" placement="top" class="fr">
                 <el-switch v-model="scope.row.status" :active-value="1" :inactive-value="0"
                   :active-color="switchActiveColor" class="mt6"></el-switch>
               </el-tooltip>
@@ -52,44 +62,46 @@
     </div>
 
     <!-- 添加城市弹窗 -->
-    <el-dialog :visible.sync="isVisible" title="添加城市" :before-close="closeDialog">
+    <el-dialog :visible.sync="isVisible" title="添加城市" :before-close="closeEditDialog">
       <el-form :model="addCityForm" label-width="100px" ref="addCityForm">
-        <el-form-item label="所属国家" prop="country" required>
-          <el-select v-model="addCityForm.country" class="width340px">
-            <el-option v-for="(value, key) in countryList" :value="key" :key="key" :label="value"></el-option>
+        <el-form-item label="所属国家" prop="countryId" required>
+          <el-select v-model="addCityForm.countryId" class="width340px">
+            <el-option v-for="item in countryList" :value="item.id" :key="item.cnName"
+              :label="item.cnName"></el-option>
           </el-select>
         </el-form-item>
 
-        <el-form-item label="城市" prop="city" :rules="[
+        <el-form-item label="城市" prop="regionCode" :rules="[
             { required: true, message: '请选择省市', trigger: ['blur', 'change'] }]">
-          <el-select v-model="addCityForm.city" placeholder="请选择省市" class="width340px">
-            <!--<el-option v-for="(value, key) in countryList" :value="key" :key="key" :label="value"></el-option>-->
-          </el-select>
+          <el-cascader :options="cityTree" v-model="city" :props="cityProps" @change="changeCity" filterable
+            placeholder="请选择省市" class="width340px">
+          </el-cascader>
         </el-form-item>
 
-        <el-form-item label="城市名称" prop="city" :rules="[
+        <el-form-item label="城市名称" prop="cityAliasName" :rules="[
           { required: true, message: '请输入城市名称', trigger: ['blur', 'change'] },
           { max: 10, message: '最大允许输入10个字', trigger: ['blur', 'change'] }]">
-          <el-input v-model="addCityForm.cityName" placeholder="请输入城市名称" class="width340px"></el-input>
+          <el-input v-model="addCityForm.cityAliasName" placeholder="请输入城市名称" class="width340px"></el-input>
         </el-form-item>
 
-        <!-- TODO 经纬度校验 -->
         <el-form-item label="经度" prop="longitude" :rules="[
-          { required: true, message: '请输入经度', trigger: ['blur', 'change'] }]">
-          <el-input v-model="addCityForm.longitude" placeholder="请输入经度" class="width340px"></el-input>
+          { required: true, message: '请输入经度', trigger: ['blur', 'change'] },
+          { validator: checkLongitude, trigger: ['blur', 'change'] }]">
+          <el-input v-model="addCityForm.longitude" placeholder="请输入经度" :maxlength="15" class="width340px"></el-input>
         </el-form-item>
 
         <el-form-item label="纬度" prop="latitude" :rules="[
-          { required: true, message: '请输入纬度', trigger: ['blur', 'change'] }]">
-          <el-input v-model="addCityForm.latitude" placeholder="请输入纬度" class="width340px"></el-input>
+          { required: true, message: '请输入纬度', trigger: ['blur', 'change'] },
+          { validator: checkLatitude, trigger: ['blur', 'change'] }]">
+          <el-input v-model="addCityForm.latitude" placeholder="请输入纬度" :maxlength="14" class="width340px"></el-input>
         </el-form-item>
 
-        <el-form-item label="展示图" prop="imgUrl" :rules="[
+        <el-form-item label="展示图" prop="url" :rules="[
           { required: true, message: '请上传展示图', trigger: ['blur', 'change'] }]">
           <lh-upload
-            :imgUrl="addCityForm.imgUrl"
+            :imgUrl="addCityForm.url"
             class="fl"
-            @uploadImg="val => addCityForm.imgUrl = val"></lh-upload>
+            @uploadImg="val => addCityForm.url = val"></lh-upload>
           <i class="el-icon-question fl theme-light-gray ml10 mt8 mr5" @click="uploadText = true"></i>
           <div v-if="uploadText" class="fl">
             <p>建议尺寸： 600PX * 460PX<br>支持格式： JPG / PNG</p>
@@ -98,7 +110,7 @@
       </el-form>
 
       <div slot="footer" class="dialog-footer">
-        <el-button @click="closeDialog">取 消</el-button>
+        <el-button @click="closeEditDialog">取 消</el-button>
         <el-button type="primary" @click="submitForm">保 存</el-button>
       </div>
     </el-dialog>
@@ -108,6 +120,7 @@
 <script>
   import tableMixins from '@/mixins/table'
   import upload from '@/components/upload'
+  import { listCountry, listCity, listCityTree, addCity, updateCity } from '@/service/city-maintain'
   export default {
     name: '',
     mixins: [tableMixins],
@@ -116,49 +129,220 @@
     },
     data() {
       return {
-        countryList: {
-          1: '中国',
-          2: '美国'
-        },
+        countryList: [],
         citySort: {
-          country: '1',
-          name: ''
+          countryId: '44', // 默认中国
+          keyword: ''
         },
         tableData: [],
+        tableLoading: true,
 
         // 添加城市弹窗
         isVisible: false,
         addCityForm: {
-          country: '1',
-          city: '',
-          cityName: '',
+          countryId: 44, // 默认中国
+          regionCode: '',
+          cityAliasName: '',
           longitude: '', // 经度
           latitude: '', // 纬度
-          imgUrl: ''
+          url: ''
+        },
+        city: [], // 级联选择城市
+        // 树状城市列表
+        cityTree: [
+          {
+            name: '广东省',
+            children: [{
+              code: '10181018',
+              name: '广州'
+            }],
+            code: '1018'
+          }, {
+            name: '北京',
+            children: [{
+              code: '10001000',
+              name: '北京市'
+            }],
+            code: '1000'
+          }
+        ],
+        cityProps: {
+          value: 'code',
+          label: 'name'
         },
         uploadText: false
       }
     },
-    mounted() {},
+    mounted() {
+      // 获取树状城市列表
+      // this.getCityTree()
+      // 获取国家列表
+      this.getCountryList()
+      // 获取城市列表
+      this.getPageData()
+    },
     methods: {
-      getPageData(page) {},
+      // 树状城市列表
+      getCityTree() {
+        listCityTree().then(res => {
+          if (res.status === 'true' && res.info) {
+            this.cityTree = res.info.children
+          }
+        })
+      },
+
+      // 选择省市
+      changeCity(val) {
+        // 省市选择器改变时存储最后一个城市code
+        if (val && val.length > 1) this.addCityForm.regionCode = val[1]
+      },
+
+      // 国家列表
+      getCountryList() {
+        listCountry().then(res => {
+          if (res.status === 'true') {
+            this.countryList = res.info
+          }
+        })
+      },
+
+      // 城市列表
+      getPageData(page) {
+        this.tableData = [{
+          id: 1,
+          regionCode: '10181018',
+          cityCode: '10181018',
+          provinceCode: '1018',
+          url: 'http://shared-office.oss-cn-shenzhen.aliyuncs.com/shared-office/img/20170921/20170921202804135Sd7.jpg',
+          countryId: 44,
+          countryName: '中国',
+          provinceName: '广东省',
+          cityName: '广州市',
+          status: 1,
+          cityAliasName: '广州',
+          longitude: 23.1955,
+          latitude: 113.3009,
+          sort: 1
+        }, {
+          id: 2,
+          regionCode: '10001000',
+          cityCode: '10001000',
+          provinceCode: '1000',
+          url: 'http://shared-office.oss-cn-shenzhen.aliyuncs.com/shared-office/img/20170921/20170921202804135Sd7.jpg',
+          countryId: 44,
+          countryName: '中国',
+          provinceName: '北京',
+          cityName: '北京市',
+          status: 1,
+          cityAliasName: '北京的',
+          longitude: 23.1955,
+          latitude: 113.3009,
+          sort: 1
+        }]
+        this.tableLoading = true
+        this.currentPage = page || this.currentPage
+        let params = Object.assign({
+          pageSize: this.pageSize,
+          pageNum: this.currentPage
+        }, this.citySort)
+        listCity(params).then(res => {
+          if (res.status === 'true' && res.info) {
+            this.pageTotal = res.info.total
+            this.tableData = res.info.result
+            this.tableLoading = false
+          } else {
+            this.$message.info(res.msg || '暂无数据')
+          }
+        })
+      },
+
+      // 添加或编辑城市
       submitForm() {
         this.$refs['addCityForm'].validate(valid => {
           if (valid) {
-            this.isVisible = false
+            let params = this.addCityForm
+            let promise = this.addCityForm.id ? updateCity(params) : addCity(params)
+            let successTips = this.addCityForm.id ? '修改成功！' : '添加成功！'
+            promise.then(res => {
+              if (res.status === 'true') {
+                this.isVisible = false
+                this.$message.success(successTips)
+                this.resetForm()
+                this.getPageData()
+              } else {
+                this.$message.error(res.msg || 'something error')
+              }
+            })
           } else {
             this.$message.error('请确认表单填写正确')
           }
         })
       },
-      closeDialog() {
+
+      // 打开编辑对话框
+      openEditDialog(id) {
+        let target = this.tableData.find(item => item.id === id)
+        this.isVisible = true
+        this.addCityForm = Object.assign({}, {
+          id: target.id,
+          countryId: target.countryId,
+          regionCode: target.regionCode,
+          cityAliasName: target.cityAliasName,
+          longitude: target.longitude,
+          latitude: target.latitude, // 纬度
+          url: target.url
+        })
+        this.city = [target.provinceCode, target.cityCode]
+      },
+
+      // 关闭编辑对话框
+      closeEditDialog() {
         this.$confirm('确认关闭？', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
+          this.resetForm()
           this.isVisible = false
         }).catch(() => {})
+      },
+
+      // 重置表单
+      resetForm() {
+        this.addCityForm = {
+          countryId: 44, // 默认中国
+          regionCode: '',
+          cityAliasName: '',
+          longitude: '', // 经度
+          latitude: '', // 纬度
+          url: ''
+        }
+        this.city = []
+        this.$nextTick(() => {
+          this.$refs['addCityForm'].clearValidate()
+        })
+      },
+
+      // 自定义校验经度
+      checkLongitude(rule, value, callback) {
+        if (value && isNaN(value)) {
+          callback(new Error('请输入数字'))
+        }
+        if (Math.abs(value) > 180) {
+          callback(new Error('请输入正确的经度（-180～180）'))
+        }
+        callback()
+      },
+
+      // 自定义校验纬度
+      checkLatitude: (rule, value, callback) => {
+        if (value && isNaN(value)) {
+          callback(new Error('请输入数字'))
+        }
+        if (Math.abs(value) > 90) {
+          callback(new Error('请输入正确的纬度（-90～90）'))
+        }
+        callback()
       }
     }
   }
