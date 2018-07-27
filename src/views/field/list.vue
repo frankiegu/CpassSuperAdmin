d5664467a6a7d3f8cc79b91d909856890175f50f
CAIKAIYI <kaiyi_c@gzleihou.cn>
2018-07-27 17:13:10 +0800

    状态筛选器调整位置

--- src/views/field/list.vue
+++ src/views/field/list.vue
@@ -2,7 +2,7 @@
   <div class="field-list">
     <lh-title></lh-title>
 
-    <div class="card-padding">
+    <div class="lh-form-box">
       <el-form :model="formData" :inline="true" class="text-right mr-10" @submit.native.prevent>
         <!--选择预约日期-->
         <el-form-item>
@@ -20,81 +20,79 @@
         </el-form-item>
       </el-form>
 
-      <!-- @#TODO 测试排序 排序规则：根据添加时间倒序显示，最近设置为对外开放的场地显示在最上方 -->
-      <el-table
-        :data="tableData"
-        :empty-text="tableEmpty"
-        :slot="tableEmpty"
-        v-loading="tableLoading"
-        @sort-change="change"
-        @filter-change="filterChange"
-        class="width100" border>
-
-        <el-table-column label="场地名称" fixed="left" align="left">
-          <template slot-scope="scope">
-            <router-link
-              :to="{path: '/field/detail', query: {id: scope.row.fieldId}}"
-              class="table-link">
-              {{ scope.row.fieldName }}
-            </router-link>
-          </template>
-        </el-table-column>
-
-        <el-table-column label="场地所属" prop="spaceName" align="left"></el-table-column>
-        <el-table-column label="所在地区" prop="address" align="left"></el-table-column>
-        <el-table-column label="场地类型" prop="fieldTypeName" align="left"></el-table-column>
-
-        <el-table-column label="历史预定数" prop="bookNum" sortable="custom" align="left" width="115"></el-table-column>
-
-        <el-table-column label="预定价格" :formatter="formatterPrice" align="left"></el-table-column>
-        <el-table-column label="联系人" prop="contact" align="left"></el-table-column>
-        <el-table-column label="联系方式" prop="phone" align="left" width="110"></el-table-column>
-
-        <el-table-column label="场地更新时间" prop="updated" align="left" width="105"></el-table-column>
-
-        <el-table-column :label="sortStatusName" prop="isOpen" width="100"
-                         :filters="[{ text: '待审核', value: 0 }, { text: '审核通过', value: 1 }]"
-                         :filter-multiple="false"
-                         filter-placement="bottom-end"
-                         column-key="statusType">
-          <template slot-scope="scope">
-            <!--待审核, 审核通过-->
-            <span v-if="scope.row.isOpen === 0">待审核</span>
-            <span v-else-if="scope.row.isOpen === 1">审核通过</span>
-          </template>
-
-        </el-table-column>
-        <el-table-column label="操作" fixed="right" align="left">
-          <template slot-scope="scope">
-            <el-tooltip
-              :content="scope.row.isOpen === 1 ? '点击停用场地' : '点击启用场地'"
-              placement="top"
-              class="margin-lr6">
-
-              <el-switch
-                v-model="scope.row.isOpen"
-                :active-value="1"
-                :inactive-value="0"
-                :active-color="switchActiveColor"
-                active-text=""
-                inactive-text=""
-                @change="handleUpdateStatus(scope.row.fieldId, scope.row.isOpen)"></el-switch>
-            </el-tooltip>
-          </template>
-        </el-table-column>
-      </el-table>
-
-      <el-pagination
-        :total="pageTotal"
-        :layout="layoutArr"
-        :page-size="pageSize"
-        :page-sizes="pageSizeArr"
-        :current-page="currentPage"
-        class="pagination-container"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        background></el-pagination>
     </div>
+
+    <div class="lh-card-main">
+      <el-tabs v-model="statusType" class="lh-form-tab mb0" @tab-click="getPageData(1)">
+        <el-tab-pane v-for="item in statusList" :label="item.label" :key="item.type" :name="item.type" />
+      </el-tabs>
+
+      <div class="card-body">
+        <!-- @#TODO 测试排序 排序规则：根据添加时间倒序显示，最近设置为对外开放的场地显示在最上方 -->
+        <el-table
+          :data="tableData"
+          :empty-text="tableEmpty"
+          :slot="tableEmpty"
+          v-loading="tableLoading"
+          @sort-change="change"
+          class="width100" border>
+
+          <el-table-column label="场地名称" fixed="left" align="left">
+            <template slot-scope="scope">
+              <router-link
+                :to="{path: '/field/detail', query: {id: scope.row.fieldId}}"
+                class="table-link">
+                {{ scope.row.fieldName }}
+              </router-link>
+            </template>
+          </el-table-column>
+
+          <el-table-column label="场地所属" prop="spaceName" align="left"></el-table-column>
+          <el-table-column label="所在地区" prop="address" align="left"></el-table-column>
+          <el-table-column label="场地类型" prop="fieldTypeName" align="left"></el-table-column>
+
+          <el-table-column label="历史预定数" prop="bookNum" sortable="custom" align="left" width="115"></el-table-column>
+
+          <el-table-column label="预定价格" :formatter="formatterPrice" align="left"></el-table-column>
+          <el-table-column label="联系人" prop="contact" align="left"></el-table-column>
+          <el-table-column label="联系方式" prop="phone" align="left" width="110"></el-table-column>
+
+          <el-table-column label="场地更新时间" prop="updated" align="left" width="105"></el-table-column>
+
+          <el-table-column label="操作" fixed="right" align="left">
+            <template slot-scope="scope">
+              <el-tooltip
+                :content="scope.row.isOpen === 1 ? '点击停用场地' : '点击启用场地'"
+                placement="top"
+                class="margin-lr6">
+
+                <el-switch
+                  v-model="scope.row.isOpen"
+                  :active-value="1"
+                  :inactive-value="0"
+                  :active-color="switchActiveColor"
+                  active-text=""
+                  inactive-text=""
+                  @change="handleUpdateStatus(scope.row.fieldId, scope.row.isOpen)"></el-switch>
+              </el-tooltip>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <el-pagination
+          :total="pageTotal"
+          :layout="layoutArr"
+          :page-size="pageSize"
+          :page-sizes="pageSizeArr"
+          :current-page="currentPage"
+          class="pagination-container"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          background></el-pagination>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
@@ -110,8 +108,21 @@
     data () {
       return {
         bookNumSort: '',
-        sortStatusName: '状态',
-        statusType: '',
+        statusType: '-1',
+        statusList: [
+          {
+            label: '全部',
+            type: '-1'
+          },
+          {
+            label: '待审核',
+            type: '0'
+          },
+          {
+            label: '审核通过',
+            type: '1'
+          }
+        ],
         formData: {
           update: '', // 更新时间
           name: ''
@@ -151,7 +162,7 @@
           bookNumSort: this.bookNumSort,
           fieldName: this.formData.name,
           storeId: this.$route.query.storeId,
-          status: this.statusType,
+          status: this.statusType !== '-1' ? this.statusType : null,
           startTime: this.formData.update ? formatTimeString(this.formData.update[0]) : null,
           endTime: this.formData.update ? formatTimeString(this.formData.update[1]) : null
         }
@@ -193,39 +204,16 @@
         //    this.$message.error(res.msg)
         //  }
         // })
-      },
-      // 筛选触发动作
-      filterChange (filters) {
-        if (filters.hasOwnProperty('statusType')) {
-          if (filters['statusType'].length === 0) {
-            this.sortStatusName = '状态'
-            this.statusType = ''
-            this.getPageData(1)
-            return;
-          }
-          if (filters['statusType'][0] === this.statusType) {
-            return;
-          }
-          console.log('filter', filters['statusType'][0])
-          switch (filters['statusType'][0]) {
-            case 1:
-              this.statusType = 1
-              this.sortStatusName = '审核通过'
-              break;
-            case 0:
-              this.statusType = 0
-              this.sortStatusName = '待审核'
-              break;
-          }
-          this.getPageData(1)
-        }
       }
     }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import "src/styles/config";
   .field-list {
+    .el-tabs__header {
+    margin-bottom: 0;
+    }
   }
 </style>
