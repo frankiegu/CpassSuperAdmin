204c83d7070a25c4aacf99f149231010b805bfdb
连明堂 <davilien@163.com>
2018-10-17 16:16:51 +0800

    CPASS-1037 漏提交

--- src/views/market/c-pass/com/quill.mixin.js
+++ src/views/market/c-pass/com/quill.mixin.js
@@ -41,7 +41,7 @@ export default {
       this.verifyImage()
     },
     verifyImage() {
-      this.verifyImg = !this.formData.bannerPath ? '请上传头像' : ''
+      this.verifyImg = !this.formData.bannerPath ? '请上传活动banner' : ''
     },
     onTextChange({ editor, html, text }) {
       // console.log('test', editor, html, text);
